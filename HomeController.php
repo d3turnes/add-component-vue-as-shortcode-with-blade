f2c16231492961e2c80a2451f251c2ea5d9e89aa
d3turnes <32837544+d3turnes@users.noreply.github.com>
2019-08-31 01:47:01 +0200

    Update HomeController.php

--- HomeController.php
+++ HomeController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Page;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller {
+	
     public function index() {
 		
-		$page = Page::where('slug', '=', 'hello-world')->first();
+	$page = Page::where('slug', '=', 'hello-world')->first();
 		
 		
-		$page->body = str_replace("&lt;","<",str_replace("&gt;",">",preg_replace('/&lt;!--\\/\\/(.*)\\/\\/--&gt;/i', '${1}', $page->body, -1)));
-		$page->body = preg_replace('/\[(.*?)\]/i','<${1}>', $page->body, -1);
+	$page->body = str_replace("&lt;","<",str_replace("&gt;",">",preg_replace('/&lt;!--\\/\\/(.*)\\/\\/--&gt;/i', '${1}', $page->body, -1)));
+	$page->body = preg_replace('/\[(.*?)\]/i','<${1}>', $page->body, -1);
 		
 		
-		return view('home', compact('page'));		
-	}
+	return view('home', compact('page'));		
+    }
+	
 }
