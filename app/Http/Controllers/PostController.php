13e8ad5141fa3c004c640bdfc32d4a3c6b222ca1
David Turnes <d3turnes@gmail.com>
2019-08-31 02:55:31 +0200

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 
-class PostController extends Controller
-{
-    public function index(Request $request) {
-		
+class PostController extends Controller {
+	
+	public function index(Request $request) {
+	
 		$posts = Post::paginate(10);
 		
 		return view('posts.index', compact('posts'));
