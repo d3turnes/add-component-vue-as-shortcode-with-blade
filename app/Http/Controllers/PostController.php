74e294ede3eeac766f04d268b746304f61775688
d3turnes <32837544+d3turnes@users.noreply.github.com>
2019-08-31 02:57:35 +0200

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,6 @@ class PostController extends Controller {
 		$posts = Post::paginate(10);
 		
 		return view('posts.index', compact('posts'));
-		
 	}
 		
 	public function show(Request $request, $slug) {
@@ -28,7 +27,6 @@ class PostController extends Controller {
 				
 			return view('posts.single', compact('page'));		
 		}
-		
 		return abort('404');
 	}
 }
